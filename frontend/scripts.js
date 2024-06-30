bef30e4fe240738457c8cbfcb10a239dd77af726
Udathu.Hari Hara Durga Vasisht Pranav <102406736+HHDVasishtPranav@users.noreply.github.com>
2024-06-30 22:40:24 +0530

    Update scripts.js

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,21 +1,3 @@
-// document.getElementById('codeForm').addEventListener('submit', async function(event) {
-//     event.preventDefault();
-
-//     const language = document.getElementById('language').value;
-//     const code = document.getElementById('code').value;
-
-//     const response = await fetch('http://localhost:3000/api/execute/', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({ language, script: code })
-//     });
-
-//     const result = await response.json();
-//     document.getElementById('output').textContent = JSON.stringify(result, null, 2);
-// });
-
 document.getElementById('codeForm').addEventListener('submit', async function (event) {
     event.preventDefault();
 
